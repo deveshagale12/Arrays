6431ce1a7705a816c783f1d5147c5f458aef0add
deveshagale@gmail.com <deveshagale@gmail.com>
2025-02-06 10:03:30 +0530

    Simple Example of Arrays

---
+++ src/Jan6/Array_Example1.java
@@ -0,0 +1,29 @@
+package Jan6;
+
+public class Array_Example1 {
+
+	public static void main(String[] args) {
+
+
+		int array1[]=new int[5];
+		
+		array1[0]=56;
+		array1[1]=45;
+		array1[2]=78;
+		array1[3]=23;
+		array1[4]=67;
+		
+		System.out.println(array1[0]);
+		System.out.println(array1[1]);
+
+		System.out.println(array1[2]);
+
+		System.out.println(array1[3]);
+
+		System.out.println(array1[4]);
+
+		
+		
+	}
+
+}
