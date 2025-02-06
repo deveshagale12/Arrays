e2b12ef90706bb24fe99d847178fac2a3a75f67b
deveshagale@gmail.com <deveshagale@gmail.com>
2025-02-06 10:09:52 +0530

    Array using Scanner...

--- src/Jan6/Array_Main.java
+++ src/Jan6/Array_Main.java
@@ -8,8 +8,17 @@ public class Array_Main {
 
 
 		Scanner sc=new Scanner(System.in);
-		
-
+		System.out.println("Enter the size of an Array: ");
+		int size=sc.nextInt();
+		int array1[]=new int[size];
+		System.out.println("Enter "+size+" Element in Array");
+		for (int i = 0; i < array1.length; i++) {
+			array1[i]=sc.nextInt();
+		}
+		System.out.println("The Array Element are : ");
+		for (int i = 0; i < array1.length; i++) {
+			System.out.println(array1[i]);
+		}
 	}
 
 }
